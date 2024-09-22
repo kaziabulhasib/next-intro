f8236b3ba8ad7b020abf1ffc6356a052257d3146
kaziabulhasib <hasibkazi420@gmail.com>
2024-09-22 11:57:52 +0530

    navbar design change

---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div className='min-h-screen'>team</div>;
+};
+
+export default page;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,30 @@ import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const links = [
+    {
+      title: "about", // Wrapped in quotes
+      path: "/about",
+    },
+    {
+      title: "contact", // Wrapped in quotes
+      path: "/contact",
+    },
+    {
+      title: "blog", // Wrapped in quotes
+      path: "/blog",
+    },
+  ];
+
   return (
     <div className='flex justify-between px-24 bg-red-400 py-6 my-4'>
       <p>Next</p>
       <ul className='flex justify-evenly gap-6'>
-        <li>
-          <Link href='/about'>About</Link>
-        </li>
-        <li>
-          <Link href='/contact'>contact</Link>
-        </li>
-        <li>
-          <Link href='/blog'>Blog</Link>
-        </li>
+        {links?.map((link) => (
+          <Link key={link.path} href={link.path}>
+            {link.title}
+          </Link>
+        ))}
       </ul>
     </div>
   );
